bb70671c02a88907872682eba2aed7392756662b
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-05-25 13:26:16 +0200

    Support indexing for tag pages.

--- www/cgi-bin/index.sh
+++ www/cgi-bin/index.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+if [[ -z "$PATH_INFO" ]]; then
+	PATH_INFO="/"
+fi
+
+if [[ "$PATH_INFO" != /* ]]; then
+	exit 1
+fi
+
 cd ..
+
+if [[ "$PATH_INFO" == "/" ]]; then
+	DIR=store
+elif [[ -d "tags$PATH_INFO" ]]; then
+	DIR="tags$PATH_INFO"
+fi
 
 cat cgi-bin/head.html
 
@@ -9,6 +23,6 @@ echo "<h1>Index</h1>"
 
 echo "<ul>"
 
-cgi-bin/index store 1 10
+cgi-bin/index "$DIR" 1 10
 
 echo "</ul>"
